111dd7e442a6cd758f5af90825281fd06649da24
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-01 05:42:34 +0530

    add string_manipulation.py

---
+++ day_02/string_manipulation.py
@@ -0,0 +1,9 @@
+
+print('Hello, Python\nHello, Python\nHello, Python')
+print('Hello, '+'Python')
+print('Hello,'+' Python')
+
+print('String Manipulation Exercise')
+print("String concatenation is done with '+' sign")
+print('e.g print("Hello"+"Python")')
+print('New line can print with  a backslash and n')
