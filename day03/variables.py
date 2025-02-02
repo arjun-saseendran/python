14139ce5136ea6ff63c63f8bf55b9f2b742cb8d8
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-02 05:30:48 +0530

    add swap number programe

--- day03/variables.py
+++ day03/variables.py
@@ -6,3 +6,12 @@ print('Length of your name is ',length)
 
 
 
+# Swap numbers 
+number_one = input('Enter the first number: ')
+number_two = input('Enter the second number: ')
+temp = number_one
+number_one = number_two
+number_two = temp
+
+print('First number is: ',number_one)
+print('Second number is: ',number_two)
