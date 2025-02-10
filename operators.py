2658a2f144fc78fb3148dac271ec85e78daa7bae
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-10 20:31:15 +0530

    add operatores.py

---
+++ operators.py
@@ -0,0 +1,93 @@
+
+# Add 
+num1 = 10
+num2 = 5
+sum = num1 + num2
+print('The answer is: ',sum)
+
+# Substract
+sub = num1 - num2
+print('The answer is: ',sub)
+
+# Mutiplication
+pro = num1 * num2
+print('The answer is: ',pro)
+
+# Division
+div = num1 / num2
+print('The answer is: ',div)
+
+# Power
+pow = num1 ** num1
+print('The answer is: ',pow)
+
+# Less than
+les = num1 < num2
+print('The answer is: ',les)
+
+# Less than or equall to
+loe = num1 <= num2
+print('The answer is: ',loe)
+
+# Greater than
+get = num1 > num2
+print('The answer is: ',get)
+
+# Greater than or equall to
+goe = num1 >= num2
+print('The answer is: ',goe)
+
+# Equal
+eql = num1 == num2
+print('The answer is: ',eql)
+
+# Not equal
+neq = num1 != num2
+print('The answer is: ',neq)
+
+# Or
+o_r = num1 == num2 | num1 != num2
+print('The answer is: ',o_r)
+
+# And
+an_d = num1 > num2 & num1 != num2
+print('The answer is: ',an_d)
+
+# Member
+text = 'hello world'
+mem = text in 'hello'
+print('The answer is: ',mem)
+
+# Not member
+ntm = text not in 'python'
+print('The answer is: ',ntm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
