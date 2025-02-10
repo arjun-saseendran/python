80dcb9b21ca28d6cadc7a30be84767c944d29748
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-10 19:34:35 +0530

    add data_structures.py

---
+++ data_structurs.py
@@ -0,0 +1,24 @@
+
+
+# Lists
+data = [1,'name', 3.5, [4] ]
+print(data)
+
+#  Sets
+nums = {5, 2, 4, 1, 3}
+print(nums)
+
+# Tuples
+coins = (1, 5, 2, 10)
+print(coins)
+
+# Dictionaries
+person = {
+        'name': 'Arjun',
+        'place': 'Cherthala'
+
+        }
+
+print(person['place'])
+
+
