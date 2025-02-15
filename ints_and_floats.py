50a31ee8681db0602e1a2cd94248379d31e6b49a
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 12:02:28 +0530

    add ints_and_floats.py

---
+++ ints_and_floats.py
@@ -0,0 +1,29 @@
+
+
+num = 20
+num2 = 3
+div = num / num2
+print('The answer is: ', div)
+
+num3 = 10.0
+add = num + num3
+print('The answer is: ', add)
+
+mul = num2 * num3
+print('The answer is: ', mul)
+
+powr = num3 ** num3
+print('The answer is: ', powr)
+
+to_int = int(num3)
+print('The number three value after converting to integer: ', to_int)
+
+rnd = round(num / num2)
+print('The divided value after rounded the number: ', rnd)
+
+num4 = 1.2
+num5 = 1
+
+rnd = round(num4 - num5, 2)
+print('The subtract value after rounded the numbers: ', rnd)
+
