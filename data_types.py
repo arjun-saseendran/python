2f03c15c97691205a3e2f1a6ff0533c947a9daff
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-19 05:38:02 +0530

    update data types

--- data_types.py
+++ data_types.py
@@ -1,13 +1,25 @@
+# Integer
+num = 10
+print('The value of the num is: ',num, 'The data type of the number is: ',type(num))
 
-num = 5
-name = 'python'
-age = 33.6
+# Float
+point = 10.0
+print('The value of the point is: ',point, 'The data type of the point is: ',type(point))
 
-type(num)
-type(name)
-type(age)
+# Boolean
+status = True
+print('The value of the status is: ',status, 'The data type of the status is: ',type(status))
 
-print(type(num))
-print(type(name))
-print(type(age))
+# String
+text = 'hello word'
+text1 = "hello word"
+text2 = ''''hello
+world'''
+text3 = """"hello
+world"""
+print('The mutiline string is: ',text2)
+print('The value of the text is: ',text, 'The type of the text is: ',type(text))
 
+# None
+data = None
+print('The value of the data is: ',data, 'The type of the data is: ',type(data))
