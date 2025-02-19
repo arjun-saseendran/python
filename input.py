f26675a598c2dc0a06bb5837e07c271aad730439
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-19 05:54:25 +0530

    add input.py

---
+++ input.py
@@ -0,0 +1,8 @@
+
+
+name = input('Enter your name: ')
+print('hello, ',name)
+doy = int(input('Enter your year of birth: '))
+current_year = 2025
+age = current_year - doy;
+print('You are ',age, 'years old.')
